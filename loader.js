8350798ecbdccb4a836b2efc2ad55ee6df40d0e7
andyjmaclean@gmail.com <andyjmaclean@gmail.com>
2013-06-26 13:21:52 +0000

    Prototype top nav

--- loader.js
+++ loader.js
@@ -1,3 +1,86 @@
-function loader(){
+var globalNav = function(){
+    
+    var topBar         = false;
+    var topBarSections = [];
 
-}
+    var initCSS = function(config){
+    	$.each(config.stylesheets, function(i, stylesheet){
+        	$('head').append('<link rel="stylesheet" href="' + stylesheet + '"/>');    		
+    	});
+    }
+    
+    var initHtml = function(config){
+    	var nav = $('<nav class="top-bar container">').prependTo('body');
+	        
+        nav.append(
+             '<ul class="title-area">'
+          +    '<li class="name">'
+          +      '<h1><a href="#">a</a></h1>'
+          +    '</li>'
+          +    '<li class="toggle-topbar menu-icon"><a href="#"><span></span></a></li>'
+          + '</ul>'
+        );
+	        
+        var topBar      = $('<section class="top-bar-section">').appendTo(nav);
+        var topBarLeft  = $('<ul class="left">').appendTo(topBar);
+        var topBarRight = $('<ul class="right">').appendTo(topBar);
+
+        topBarLeft.append('<li class="divider"></li>');
+        
+		$.each(config.row1.left, function(key, val) {				
+	        topBarLeft.append('<li class="divider"></li>');
+	        topBarLeft.append('<li><a href="#">' + val + '</a></li>');
+		});
+
+		topBarRight.append('<li class="divider"></li>');
+
+		$.each(config.row1.right, function(key, val){
+			topBarRight.append('<li class="divider"></li>');
+			
+	        if(typeof val == 'string'){
+	        	topBarRight.append('<li><a href="#">' + val + '</a></li>');		        	
+	        }
+	        else if(typeof val == 'object'){
+		        var menu = $('<ul class="dropdown">').appendTo(
+			       $('<li class="has-dropdown"><a href="#">' + val.title + '</a></li>')
+			       .appendTo(topBarRight)		        		
+		        );
+	        		
+	        	$(val.items).each(function(i, ob){
+	        		menu.append('<li><a href="#">' + ob + '</a></li>');
+	        	});
+	        }
+		});
+		
+    };
+    
+    var initJS = function(config){
+    	var recursiveLoad = function(index){
+ 		   if(config.scripts[index]){
+ 	          $.getScript(config.scripts[index], function(){
+            	recursiveLoad(index + 1);        		   
+ 	          }).fail(function(jqxhr, settings, exception) {
+ 	        	console.log( "Error: " + exception );
+ 	          });
+ 		   }
+       	   else{
+     	      $(document).foundation();
+       	   }
+    	};
+    	recursiveLoad(0);   	
+    }
+    
+    return {
+        init:function(config){
+        	$(document).ready(function(){
+        		$.getJSON(config, function(data) {
+	        		initCSS(data.css);
+	                initHtml(data.html);
+	                initJS(data.js);
+        		}).fail(function(e) { console.log( "Config error [" + config + "]: " + e.statusText ); })
+        	});
+        }
+    }
+}();
+
+
